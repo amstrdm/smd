e08277092f1b5ae563ec0d4fa6311e3207df154a
amstrdm <a.caserta@proton.me>
2025-09-03 16:52:01 +0200

    added delete endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import latest, preview, random, upload
+from routes import latest, manage, preview, random, upload
 
 app = FastAPI()
 
@@ -15,8 +15,9 @@ app.add_middleware(
 )
 
 app.include_router(upload.router, prefix="/api")
-app.include_router(preview.router, prefix="/api")
+app.include_router(manage.router, prefix="/api")
 
 # Routes that return video elements
+app.include_router(preview.router, prefix="/api")
 app.include_router(random.router, prefix="/api/videos")
 app.include_router(latest.router, prefix="/api/videos")
