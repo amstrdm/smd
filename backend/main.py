08333ce1c38ba61af5cf28de62c4eeecc7d9f666
amstrdm <a.caserta@proton.me>
2025-09-03 04:13:07 +0200

    added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes import latest, preview, random, upload
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+origins = ["http://localhost", "http://localhost:5173"]
 
-templates = Jinja2Templates(directory="templates")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload.router, prefix="/api")
 app.include_router(preview.router, prefix="/api")
